4e130e19ace7c9041f61155d2f2489c31cfe7a75
kittu2karthik <kittu2karthik@gmail.com>
2025-11-26 01:38:51 +0530

    Added Selection.js and Updated the code in App.js, ColourSelector.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,17 @@
+import React, { useState } from "react";
 
+function Selection({ applyColor }) {
+  const [style, setStyle] = useState({ background: "" });
+
+  function handleColor() {
+    applyColor(setStyle);
+  }
+
+  return (
+    <div onClick={handleColor} style={style}>
+      Selection
+    </div>
+  );
+}
+
+export default Selection;
